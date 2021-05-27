058aa7cf07c0c981d05da45d6ae4e89de88f7535
alexl313 <84152571+alexl313@users.noreply.github.com>
2021-05-27 14:29:45 -0500

    Add files via upload

---
+++ ClassThing/ClassThing/ViewControler2.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControler2.swift
+//  ClassThing
+//
+//  Created by user167306 on 1/5/21.
+//  Copyright © 2021 user167306. All rights reserved.
+//
+
+import UIKit
+
+class ViewControler2: NSObject {
+
+}
